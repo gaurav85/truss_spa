e8bb53c857f32bb0f0acd6d6ba78075a043eba1a
Gaurav Sharma <gsharma@convoso.com>
2022-02-14 11:39:50 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test-app';
+  showLoader: boolean = false;
+  planets: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.loadPlanets();
+  }
+
+  loadPlanets() {
+    let that = this;
+    this.showLoading();
+    this.http.get("https://swapi.dev/api/planets/").subscribe((data: any) => {
+      that.hideLoading();
+      console.log(data);
+      if(data && data.hasOwnProperty('count') && data.count > 0 && data.hasOwnProperty('results') && data.results) {
+        that.planets = data.results.sort(function(a: any, b: any) {
+          return that.compareStrings(a.name, b.name);
+        })
+      }
+    }, (error: any) => {
+      that.hideLoading();
+      console.log(error);
+      alert('There was a problem loading the list of planets!!!');
+    });
+  }
+
+  showLoading() {
+    this.showLoader = true;
+  };
+
+  hideLoading() {
+    this.showLoader = false;
+  };
+
+  filterColumnVal(val: any) {
+    if(val == "unknown") {
+      return '?';
+    }
+    else {
+      return val;
+    }
+  }
+
+  calculateWaterSurfaceArea(waterPercentage: any, diameter: any) {
+    if(waterPercentage == '?' || diameter == '?') {
+      return '?';
+    }
+
+    // sphere surface area is 4 times pi times radius squared
+    let totalArea = 4 * Math.PI * Math.pow((diameter / 2), 2);
+    return this.numberWithSpaces(Math.round((waterPercentage / 100) * totalArea));
+  }
+
+  numberWithSpaces(val: any) {
+    return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  }
+
+  compareStrings(a: string, b: string) {
+    // Assuming you want case-insensitive comparison
+    a = a.toLowerCase();
+    b = b.toLowerCase();
+  
+    return (a < b) ? -1 : (a > b) ? 1 : 0;
+  }
 }
